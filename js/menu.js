be34015c39e11298e8a975280bf8a3b0010c1002
Artur Pokrywka <pokrywkaart@gmail.com>
2019-04-01 21:48:15 +0200

    Extract menu array from main js file

---
+++ js/menu.js
@@ -0,0 +1,51 @@
+"use strict";
+var menu = [ 
+    {
+    name: 'Margherita',
+    ingredients: [' ser', ' pomidor'], 
+        s: {
+            price: '17,00',
+            size: 25
+        },
+        m: {
+            price: '24,50',
+            size: 35
+        },
+        l: {
+            price: '31,80',
+            size: 45
+        },                     
+    },
+    {
+    name: 'Funghi',
+    ingredients: [' ser', ' pomidor', ' pieczarki'], 
+        s: {
+            price: '18,20',
+            size: 25
+        },
+        m: {
+            price: '26,00',
+            size: 35
+        },
+        l: {
+            price: '32,60',
+            size: 45
+        },                         
+    },
+    {
+        name: 'Salami',
+        ingredients: [' ser', ' pomidor', ' salami'], 
+            s: {
+                price: '19,20',
+                size: 25
+            },
+            m: {
+                price: '27,70',
+                size: 35
+            },
+            l: {
+                price: '35,30',
+                size: 45
+            },                         
+        },
+];
